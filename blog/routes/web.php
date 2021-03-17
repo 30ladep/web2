662e454f3246e82dfec3b56a2af6e1963de86ddb
Đoàn Minh Trí <tri.minh.it.0297@gmail.com>
2021-03-17 10:44:20 +0700

    fisrt update

---
+++ blog/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/trangchu', function () {
+    return view('trangchu');
+});
+Route::get('/gioithieu', function () {
+    return view('gioithieu');
+});
+Route::get('/lienhe', function () {
+    return view('lienhe');
+});
