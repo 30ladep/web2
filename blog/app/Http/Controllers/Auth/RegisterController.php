e5ed0e7295cd86d1a98c824de28a590d6be16e99
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-05-26 09:50:02 +0700

    fix register error

--- blog/app/Http/Controllers/Auth/RegisterController.php
+++ blog/app/Http/Controllers/Auth/RegisterController.php
@@ -55,14 +55,11 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'phone'=>['required','string','min:8','max:10'],
-            'repassword' => ['required', 'string', 'min:8', 'confirmed','same:password'],
-            
+          
+        
         ]);
     }
 
-    public function message(){
-        
-    }
     /**
      * Create a new user instance after a valid registration.
      *@
@@ -78,7 +75,6 @@ class RegisterController extends Controller
             'type_user_id' => 1,
             'role_id' =>1,
         ]);
-        
     }
 
         
